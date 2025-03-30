b44329390ac88865e59c37be9bb05a4b38f87650
Grisbee <pawel.sliwkaa1@gmail.com>
2025-03-30 18:54:04 +0200

    added mutex and resolved deadlocks

--- Jedzacy_filozofowie.cpp
+++ Jedzacy_filozofowie.cpp
@@ -1,54 +1,136 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
 #include <thread>
 #include <vector>
+#include <atomic>
 #include <chrono>
 #include <random>
+#include <string>
 
-// Enum for philosopher states
+// Custom mutex implementation
+class CustomMutex {
+private:
+    std::atomic<bool> locked;  
+
+public:
+    CustomMutex() : locked(false) {}
+
+    void lock() {
+        bool expected = false;
+        while (!locked.compare_exchange_weak(expected, true)) {
+            expected = false;
+            std::this_thread::yield();
+        }
+    }
+    bool try_lock() {
+        bool expected = false;
+        return locked.compare_exchange_strong(expected, true);
+    }
+
+    void unlock() {
+        locked.store(false);
+    }
+};
+
+class ConsoleLock {
+private:
+    CustomMutex mutex;
+
+public:
+    void print(const std::string& message) {
+        mutex.lock();
+        std::cout << message << std::endl;
+        mutex.unlock();
+    }
+};
+
+// Synchronizing the console 
+ConsoleLock console;
+
+// Enum for philosopher state
 enum class Current_State {
     Thinking,
     Eating,
     Hungry,
 };
 
-// Philosopher class
+// Class representing a philosopher
 class Philosopher {
 private:
     int id;
+    int num_philosophers;
     Current_State state;
+    std::vector<CustomMutex>& forks;
+
     std::mt19937 rng;
     std::uniform_int_distribution<int> think_dist;
     std::uniform_int_distribution<int> eat_dist;
 
+// Trying to pick up the forks
+    int leftFork() const {
+        return id;
+    }
+
+    int rightFork() const {
+        return (id + 1) % num_philosophers;
+    }
+
 public:
-    Philosopher(int philosopherId)
+    Philosopher(int philosopherId, int total_philosophers, std::vector<CustomMutex>& forks_ref)
         : id(philosopherId),
+        num_philosophers(total_philosophers),
         state(Current_State::Thinking),
+        forks(forks_ref),
         rng(std::random_device{}()),
-        think_dist(1000, 3000),  
-        eat_dist(1000, 2000)    
+        think_dist(1000, 3000),
+        eat_dist(1000, 2000)
     {}
 
-    // Dining function
     void dine() {
-        for (int i = 0; i < 5; ++i) {  // Each philosopher eats 5 times 
+        for (int i = 0; i < 5; ++i) {  
             think();
             getHungry();
+            pickUpForks();
             eat();
+            putDownForks();
         }
     }
 
-    // Thinking function
+    // Thinking function 
     void think() {
         setState(Current_State::Thinking);
         reportState();
         std::this_thread::sleep_for(std::chrono::milliseconds(think_dist(rng)));
     }
 
-    // Becoming hungry function
+    // Hunger reporting function
     void getHungry() {
         setState(Current_State::Hungry);
         reportState();
+    }
+
+    // Function for picking up forks (deadlock resolved with assymetry: the last philosopher picks up the forks in a reversed manner)
+    void pickUpForks() {
+        if (id == num_philosophers - 1) {
+            console.print("Philosopher " + std::to_string(id) + " is trying to pick up the right fork");
+            forks[rightFork()].lock();
+
+            console.print("Philosopher " + std::to_string(id) + " picked up right fork");
+            console.print("Philosopher " + std::to_string(id) + " is trying to pick up the left fork");
+            forks[leftFork()].lock();
+
+            console.print("Philosopher " + std::to_string(id) + " picked up left fork");
+        }
+        else {
+            console.print("Philosopher " + std::to_string(id) + " is trying to pick up the left fork");
+            forks[leftFork()].lock();
+
+            console.print("Philosopher " + std::to_string(id) + " picked up left fork");
+            console.print("Philosopher " + std::to_string(id) + " is trying to pick up the right fork");
+            forks[rightFork()].lock();
+
+            console.print("Philosopher " + std::to_string(id) + " picked up right fork");
+        }
     }
 
     // Eating function
@@ -58,6 +140,25 @@ public:
         std::this_thread::sleep_for(std::chrono::milliseconds(eat_dist(rng)));
     }
 
+    // Function for putting down forks
+    void putDownForks() {
+        if (id == num_philosophers - 1) {
+            forks[leftFork()].unlock();
+            console.print("Philosopher " + std::to_string(id) + " put down left fork");
+
+            forks[rightFork()].unlock();
+            console.print("Philosopher " + std::to_string(id) + " put down right fork");
+        }
+        else {
+            forks[rightFork()].unlock();
+            console.print("Philosopher " + std::to_string(id) + " put down right fork");
+
+            forks[leftFork()].unlock();
+            console.print("Philosopher " + std::to_string(id) + " put down left fork");
+        }
+    }
+
+    // Functions for modifying the states
     void setState(Current_State newState) {
         state = newState;
     }
@@ -75,19 +176,36 @@ public:
             stateStr = "eating";
             break;
         }
-        std::cout << "Philosopher " << id << " is " << stateStr << std::endl;
+
+        console.print("Philosopher " + std::to_string(id) + " is " + stateStr);
     }
 };
 
 int main(int argc, char* argv[]) {
-    int numPhilosophers = 5;
+    int numPhilosophers = 5; //default 5
+
+    if (argc > 1) {
+        try {
+            numPhilosophers = std::stoi(argv[1]);
+            if (numPhilosophers <= 0) {
+                std::cerr << "Number of philosophers must be greater than zero!" << std::endl;
+                return 1;
+            }
+        }
+        catch (const std::exception& e) {
+            std::cerr << "Invalid argument: " << e.what() << std::endl;
+            return 1;
+        }
+    }
 
     std::cout << "Dining Philosophers Problem Simulation" << std::endl;
     std::cout << "Number of philosophers: " << numPhilosophers << std::endl;
 
+    std::vector<CustomMutex> forks(numPhilosophers);
+
     std::vector<Philosopher> philosophers;
     for (int i = 0; i < numPhilosophers; i++) {
-        philosophers.emplace_back(i);
+        philosophers.emplace_back(i, numPhilosophers, forks);
     }
 
     std::vector<std::thread> threads;
